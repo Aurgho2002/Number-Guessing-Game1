dc884b967596a73da7e1121c8fa41808ed059090
Aurgho2002 <146772003+Aurgho2002@users.noreply.github.com>
2023-10-04 20:44:22 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,7 @@
+public class loop {
+    public static void main(String args[]){
+       float result =9.0f/2;
+        System.out.println(result);
+    }
+}
+
---
+++ pracite1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+// Swapping value :: md-1;
+/* public class pracite1 {
+    public static void main(String args[]){
+             int a=10, b=20;
+        System.out.println("Before swapping values are ...a=" +a+ " b="+b );
+
+        int t=a;
+                a=b;
+                b=t;
+        System.out.println("After swapping values are ...a=" +a+ " b="+b );
+
+
+    }
+} */
+
+
+
+
+
+
+
